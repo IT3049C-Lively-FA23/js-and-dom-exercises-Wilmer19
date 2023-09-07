238ddda9abb6d19661396d06d9e6306e7cacb281
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-09-07 17:42:11 -0400

    Sum the items of an array method

--- exercises/array-exercises/js/array.js
+++ exercises/array-exercises/js/array.js
@@ -20,7 +20,7 @@ class ArrayFunctions {
    */
   sum(arr) {
     // Hint: use the reduce method
-    return ;
+    return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
   }
 
   /**
