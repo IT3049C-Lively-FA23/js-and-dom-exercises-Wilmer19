059cf7f3686ec0e40e3420d6e173d24e7ab0d2e3
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-09-07 19:24:13 -0400

    2 constants with references to password input and reveal button

--- exercises/password-reveal/app.js
+++ exercises/password-reveal/app.js
@@ -1,7 +1,9 @@
 // 🤖: create 2 constants with references to the password input and the reveal button from the DOM
-
+const passwordInput = document.querySelector("password");
+const revealButton = document.querySelector("submit")
 
 // 🤖: Create an Event Listener on the password reveal button
+
 // 🤖: The event listener should toggle the password field attribute between `password` and `text` to change the visibility of the password field content.
 
 
