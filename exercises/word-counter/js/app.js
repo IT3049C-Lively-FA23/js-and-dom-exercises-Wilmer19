07c38deaba6bf690f96d236e530d07868f83583b
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-09-08 14:43:12 -0400

    Updated stats constant and created event listener on textarea input and update the stats section

--- exercises/word-counter/js/app.js
+++ exercises/word-counter/js/app.js
@@ -1,8 +1,16 @@
 // 🤖: create 2 constants with references to textarea input and the stats section from the DOM
 const textArea = document.querySelector("textarea");
-const stats = document.getElementsById("stat");
+const stats = document.getElementById("stat");
 
 
 // 🤖: Create an Event Listener on textarea input
 // 🤖: The event handler should update the stats section with the number of words and characters in the textarea input.
 // 🤖: You will need to utilize the split method to get the words count.
+
+textArea.addEventListener("input", function (e) {
+  words = textArea.value.trim().split(" ").length;
+  chars = textArea.value.trim().length;
+
+stats.innerHTML = "You've written " + words + " words and " + chars + " characters.";
+
+});
